8fe71caa7c88879fdbea0278cdb241ddc7201e37
LePew <freed@free.fr>
2017-03-29 11:01:45 +0200

    Chronological sort, Nested layers support, Speed optimization for range slider
    - Feature: Chronological order sort for time values inside the plugin
    - Fix: now works if the layer sent to SliderControl contains a layer of markers (nested layers)
    - Optimization: Real speed gain when dealing with a large number of markers for a range slider
    To Do: 
    - optimize rendering of markers of "follow" and "default"
    - localize the plugin and bring css customization

--- SliderControl.js
+++ SliderControl.js
@@ -1,3 +1,12 @@
+Array.prototype.keySort = function(key, desc){ // Sort time info
+  
+  this.sort(function(a, b) {
+    var result = desc ? (parseInt(a[key].time) < parseInt(b[key].time)) : (parseInt(a[key].time) > parseInt(b[key].time));
+    return result ? 1 : -1;
+  });
+  return this;
+}
+
 L.Control.SliderControl = L.Control.extend({
     options: {
         position: 'topright',
@@ -5,7 +14,7 @@ L.Control.SliderControl = L.Control.extend({
         timeAttribute: 'time',
         isEpoch: false,     // whether the time attribute is seconds elapsed from epoch
         startTimeIdx: 0,    // where to start looking for a timestring
-        timeStrLength: 19,  // the size of  yyyy-mm-dd hh:mm:ss - if millis are present this will be larger
+        timeStrLength: 21,  // the size of  yyyy-mm-dd hh:mm:ss - if millis are present this will be larger
         maxValue: -1,
         minValue: 0,
         showAllOnStart: false,
@@ -24,7 +33,7 @@ L.Control.SliderControl = L.Control.extend({
 
     extractTimestamp: function(time, options) {
         if (options.isEpoch) {
-            time = (new Date(parseInt(time))).toString(); // this is local time
+            time = (new Date(parseInt(time))).toLocaleString(); // this is local time
         }
         return time.substr(options.startTimeIdx, options.startTimeIdx + options.timeStrLength);
     },
@@ -58,7 +67,7 @@ L.Control.SliderControl = L.Control.extend({
         $(document).mouseup(function () {
             map.dragging.enable();
             //Hide the slider timestamp if not range and option alwaysShowDate is set on false
-            if (options.range || !options.alwaysShowDate) {
+            if (!options.range || !options.alwaysShowDate) {
                 $('#slider-timestamp').html('');
             }
         });
@@ -70,9 +79,17 @@ L.Control.SliderControl = L.Control.extend({
         if (this._layer) {
             var index_temp = 0;
             this._layer.eachLayer(function (layer) {
-                options.markers[index_temp] = layer;
-                ++index_temp;
+		layer.eachLayer(function (layer2) { //Maybe we should check if there are more layers?		  
+		   options.markers[index_temp]=layer2;
+		   ++index_temp;		  
+		});
+		if(options in layer) { //ajouté par fred 24/02/2017 pour les erreurs
+		  options.markers[index_temp] = layer;		  
+		  ++index_temp;
+		}
             });
+	    
+	    options.markers.keySort('options',false); //sort markers in chronological order
             options.maxValue = index_temp - 1;
             this.options = options;
         } else {
@@ -98,6 +115,9 @@ L.Control.SliderControl = L.Control.extend({
             if(_options.range) _options.values = [_options.minValue,_options.maxValue];
             else _options.value = _options.maxValue;
         }
+        this.options.oldminpos=_options.minValue; //saving displayed min-max range
+	this.options.oldmaxpos=index_start;
+	this.options.fg = L.featureGroup();
         $("#leaflet-slider").slider({
             range: _options.range,
             value: _options.value,
@@ -107,13 +127,17 @@ L.Control.SliderControl = L.Control.extend({
             step: 1,
             slide: function (e, ui) {
                 var map = _options.map;
-                var fg = L.featureGroup();
+                //var fg = L.featureGroup();
+		var fg = _options.fg;
                 if(!!_options.markers[ui.value]) {
                     // If there is no time property, this line has to be removed (or exchanged with a different property)
                     if(_options.markers[ui.value].feature !== undefined) {
                         if(_options.markers[ui.value].feature.properties[_options.timeAttribute]){
-                            if(_options.markers[ui.value]) $('#slider-timestamp').html(
-                                _extractTimestamp(_options.markers[ui.value].feature.properties[_options.timeAttribute], _options));
+			//Would be better to localize this part and/or add a .css file for personalisation
+			   if(_options.markers[ui.value]) $('#slider-timestamp').html("Fr: "+
+                                _extractTimestamp(_options.markers[ui.values[0]].feature.properties[_options.timeAttribute], _options)+
+										     "<br>To: "+
+				_extractTimestamp(_options.markers[ui.values[1]].feature.properties[_options.timeAttribute], _options));
                         }else {
                             console.error("Time property "+ _options.timeAttribute +" not found in data");
                         }
@@ -126,21 +150,57 @@ L.Control.SliderControl = L.Control.extend({
                             console.error("Time property "+ _options.timeAttribute +" not found in data");
                         }
                     }
-                    
+                    //console.log(ui.values);
                     var i;
-                    // clear markers
-                    for (i = _options.minValue; i <= _options.maxValue; i++) {
-                        if(_options.markers[i]) map.removeLayer(_options.markers[i]);
-                    }
                     if(_options.range){
                         // jquery ui using range
-                        for (i = ui.values[0]; i <= ui.values[1]; i++){
-                           if(_options.markers[i]) {
+			// Only add or delete selected markers
+                        if(ui.values[0]>_options.oldminpos) { // Delete markers
+			  for (i = _options.oldminpos; i <= ui.values[0]; i++) {
+			    if(_options.markers[i]) {
+			      map.removeLayer(_options.markers[i]);
+			      fg.removeLayer(_options.markers[i]);
+			    }
+			  }
+			  _options.oldminpos=ui.values[0];
+			}
+			
+			if(ui.values[0]<_options.oldminpos) { // add missing markers
+			  for (i=ui.values[0]; i<=_options.oldminpos; i++) {
+			    if(_options.markers[i]) {
                                map.addLayer(_options.markers[i]);
                                fg.addLayer(_options.markers[i]);
-                           }
-                        }
+                            }
+			  }
+			  _options.oldminpos=ui.values[0];
+			}
+			
+			if(ui.values[1]>_options.oldmaxpos) { // add missing markers
+			  for(i=_options.oldmaxpos;i<=ui.values[1];i++) {
+			    if(_options.markers[i]) {
+                               map.addLayer(_options.markers[i]);
+                               fg.addLayer(_options.markers[i]);
+                            }
+			  }			
+			  _options.oldmaxpos=ui.values[1];
+			}
+			
+			if(ui.values[1]<_options.oldmaxpos) { // Delete markers
+			  for(i=ui.values[1];i<=_options.oldmaxpos;i++) {
+			    if(_options.markers[i]) {
+			      map.removeLayer(_options.markers[i]);
+			      fg.removeLayer(_options.markers[i]);
+			    }
+			  }
+			  _options.oldmaxpos=ui.values[1];
+			}
+			
                     }else if(_options.follow){
+			// clear markers. We could probably optimize this!
+			for (i = _options.minValue; i <= _options.maxValue; i++) {
+			  if(_options.markers[i]) map.removeLayer(_options.markers[i]);
+			}
+			
                         for (i = ui.value - _options.follow + 1; i <= ui.value ; i++) {
                             if(_options.markers[i]) {
                                 map.addLayer(_options.markers[i]);
@@ -148,6 +208,10 @@ L.Control.SliderControl = L.Control.extend({
                             }
                         }
                     }else{
+			// clear markers. We could probably optimize this!
+			for (i = _options.minValue; i <= _options.maxValue; i++) {
+			  if(_options.markers[i]) map.removeLayer(_options.markers[i]);
+			}
                         for (i = _options.minValue; i <= ui.value ; i++) {
                             if(_options.markers[i]) {
                                 map.addLayer(_options.markers[i]);
@@ -166,8 +230,16 @@ L.Control.SliderControl = L.Control.extend({
         if (!_options.range && _options.alwaysShowDate) {
             $('#slider-timestamp').html(_extractTimeStamp(_options.markers[index_start].feature.properties[_options.timeAttribute], _options));
         }
+        if(_options.range) { // Should localize and/or add .css file for this...
+	  $('#slider-timestamp').html("Fr: "+
+                                _extractTimestamp(_options.markers[_options.minValue].feature.properties[_options.timeAttribute], _options)+
+										     "<br>To: "+
+				_extractTimestamp(_options.markers[_options.maxValue].feature.properties[_options.timeAttribute], _options));
+	}
+        
         for (i = _options.minValue; i <= index_start; i++) {
             _options.map.addLayer(_options.markers[i]);
+	    _options.fg.addLayer(_options.markers[i]);
         }
     }
 });
